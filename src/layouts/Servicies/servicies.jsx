fdbdc3eb3cc2a18ba691ea7ccb0eb84c525788dc
alelabas <alelabas208@gmail.com>
2025-04-03 16:10:52 -0300

    Add new car and client images; replace JPGs with WEBP format for better performance

--- src/layouts/Servicies/servicies.jsx
+++ src/layouts/Servicies/servicies.jsx
@@ -6,28 +6,28 @@ export default function Servicies(){
             <h2>Our servicies</h2>
             <div className="servicies-container">
                 <div className="service-container">
-                    <img src="src/assets/CarImages/mustang-blanco-1.jpg" alt="" />
+                    <img src="src/assets/CarImages/mustang-blanco-1.webp" alt="" />
                     <div className="service-text-container">
                         <h3>Sports Cars Rental</h3>
                         <p>We offer the best cars for the best price in the market. You can enjoy the big engines while driving with the roof down and enjoy the breeze of Miami.</p>
                     </div>
                 </div>
                 <div className="service-container">
-                    <img src="src/assets/CarImages/mustangs-2.jpg" alt="" />
+                    <img src="src/assets/CarImages/mustangs-2.webp" alt="" />
                     <div className="service-text-container">
                         <h3>24/7 Pick-Up and Drop-Off</h3>
                         <p>We can Deliver and Pick-Up the cars whenever it's comfortable for <strong>you</strong>. Even early in the morning or late in the night</p>
                     </div>
                 </div>
                 <div className="service-container">
-                    <img src="src/assets/CarImages/mustangs-3.jpg" alt="" />
+                    <img src="src/assets/CarImages/mustangs-3.webp" alt="" />
                     <div className="service-text-container">
                         <h3>Airport Pick-Up and Drop-Off</h3>
                         <p>You can forget about arriving at Miami and calling a taxi or uber. We will deliver and pick-up the car straight <strong>out of your gate</strong></p>
                     </div>
                 </div>
                 <div className="service-container">
-                    <img src="src/assets/CarImages/mustangs-2.jpg" alt="" />
+                    <img src="src/assets/CarImages/mustangs-2.webp" alt="" />
                     <div className="service-text-container">
                         <h3>Renting Combos</h3>
                         <p>Do you want to rent a convertible but you can't fit your luggage? No worries, we can arrange both a SUV or Van and a convertible combo</p>
