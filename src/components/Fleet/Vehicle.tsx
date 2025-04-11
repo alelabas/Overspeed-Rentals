aedcf2e436262afecea3a505af08e66a8764524c
alelabas <alelabas208@gmail.com>
2025-04-11 16:35:14 -0300

    Enhance Fleet and Servicies styles; add hover effects and button interactions for improved user experience

--- src/components/Fleet/Vehicle.tsx
+++ src/components/Fleet/Vehicle.tsx
@@ -1,12 +1,20 @@
 import React from "react";
-import { IoMdPeople } from "react-icons/io";
+import { IoMdPeople, IoMdClose } from "react-icons/io";
 import { MdLuggage } from "react-icons/md";
 import { IconContext } from "react-icons/lib"
 import { LiaSuitcaseSolid } from "react-icons/lia";
 import { LuFuel } from "react-icons/lu";
 import { Cars } from "../../types/types";
+import { useRef } from "react";
 
 export default function Vehicle({ vehicle }: {vehicle: Cars}) {
+
+	const refDialog = useRef<HTMLDialogElement>(null)
+ 
+	const handleClick = () => {
+		refDialog.current?.showModal()
+	}
+
 	return (
 		<div key={vehicle.id} className="fleet-card">
 			<img src={vehicle.images[0]} alt={vehicle.model} />
@@ -14,7 +22,7 @@ export default function Vehicle({ vehicle }: {vehicle: Cars}) {
 				{vehicle.make} {vehicle.model}
 			</h3>
 			<div className="vehicle-specifications">
-				<IconContext.Provider value={{ size: "20px", color: "black" }}>
+				<IconContext.Provider value={{ size: "20px", color: "#ff3c3c" }}>
 					<span className="vehicle-specs">
 						<IoMdPeople />
 						<p>{vehicle.passengers}</p>
@@ -37,6 +45,23 @@ export default function Vehicle({ vehicle }: {vehicle: Cars}) {
 				From <span className="vehicle-price">${vehicle.price}/day</span>{" "}
 				- <span className="discount-price">${vehicle.price + 40}</span>
 			</p>
+			<div className="button-view-more">
+				<button onClick={handleClick}>See more</button>
+			</div>
+
+			<dialog ref={refDialog} className="dialog-modal" closedBy="any">
+				<div className="dialog-content">
+					<div className="close-menu">
+						<form method="dialog"><button className="close-button"><IoMdClose size={'25px'}/></button></form>
+					</div>
+					
+					<div className="main-content">
+						<h3>{vehicle.make} {vehicle.model}</h3>
+						<img src={vehicle.images[1]} alt={vehicle.model} />
+					</div>
+				</div>	
+			</dialog>
+
 		</div>
 	)
 }
