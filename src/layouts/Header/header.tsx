a8e54a4441af8bb7b4c43355d813915ea0b04ad3
alelabas <alelabas208@gmail.com>
2025-04-30 19:28:26 -0300

    add select for languages and some styles

--- src/layouts/Header/header.tsx
+++ src/layouts/Header/header.tsx
@@ -7,11 +7,24 @@ export default function Header() {
   
   const changeLanguage = useContext(langContext);
 
+  const handleLanguageChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedLanguage = event.target.value;
+    changeLanguage?.changeLanguage(selectedLanguage);
+  };
+
   return (
     <header className="header">
-      <a href=""><img src="assets/logo-4-png.png" alt="logo sitio web" /></a>
-      <button onClick={() => changeLanguage?.changeLanguage("esp")}>Espanol</button>
-      <button onClick={() => changeLanguage?.changeLanguage("eng")}>English</button>
+      <div className="header-content">
+        <div className="logo-container">
+          <a href=""><img src="assets/logo-4-png.png" alt="logo sitio web" /></a>
+        </div>
+        <div className="language-buttons">
+          <select name="language-select" onChange={handleLanguageChange} defaultValue="esp">
+            <option value="esp">Spanish</option>
+            <option value="eng">English</option>
+          </select>
+        </div>
+      </div>
     </header>
   );
 }
